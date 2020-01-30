6c59c0e39fec44b3f1b7ddfb7953a0f300050d50
MarioGN <mario.geroldineto@gmail.com>
2020-01-30 12:08:33 -0300

    Form adicionado ao template

--- dynamicfleet/reservas/tests/test_view_reserve_vehicle.py
+++ dynamicfleet/reservas/tests/test_view_reserve_vehicle.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from dynamicfleet.reservas.forms import ReserveForm
 
 
 class ReserveVehicleGet(TestCase):
@@ -11,3 +12,21 @@ class ReserveVehicleGet(TestCase):
     
     def test_template(self):
         self.assertTemplateUsed(self.response, 'reservas/reserve_vehicle.html')
+
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertIsInstance(form, ReserveForm)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_html(self):
+        tags = (('<form', 1),
+                ('<input', 3),
+                ('<select', 1),
+                ('type="text"', 2),
+                ('type="submit"', 1))
+
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.response, text, count)
--- dynamicfleet/reservas/views.py
+++ dynamicfleet/reservas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from dynamicfleet.reservas.forms import ReserveForm
 
 
 def reserve_vehicle(request):
-    return render(request, 'reservas/reserve_vehicle.html')
+    return render(request, 
+                  'reservas/reserve_vehicle.html', 
+                  {'form': ReserveForm()})
