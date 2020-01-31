b63df3bbe7c654c050c12242d59146ae2334797d
MarioGN <mario.geroldineto@gmail.com>
2020-01-30 23:48:30 -0300

    Realizando a filtragem de reservas

--- dynamicfleet/reservas/views.py
+++ dynamicfleet/reservas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, HttpResponse
 from dynamicfleet.reservas.forms import ReserveForm, FilterReserveForm
 from dynamicfleet.reservas.models import Reserve
 from dynamicfleet.veiculos.models import Vehicle
+from django.db.models import Q
 
 
 def reserve_vehicle(request, pk=None):
@@ -29,6 +30,23 @@ def reserve_vehicle(request, pk=None):
 
 def reserves_list(request):
     reserves = Reserve.objects.all()
+
+    if request.method == 'GET':
+        start = request.GET.get('start')
+        end = request.GET.get('end')
+
+        if start and end:
+            form = FilterReserveForm(request.GET)
+            form.full_clean()
+            
+            start = form.cleaned_data.get('start')
+            end = form.cleaned_data.get('end')
+
+            reserves = Reserve.objects.filter(
+                            Q(start__range=(start, end)) &
+                            Q(end__range=(start, end))
+                        ).distinct()
+
     
     return render(request, 
                   'reservas/reserves_list.html', 
